d57263678d9b9ebf4baab18f81f9ff334b0c7eaf
Chat Sumlin <chat@chatsumlin.com>
2024-12-12 17:31:54 -0500

    Initial commit of vite migrations

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import AppleMusicLogo from "@/assets/apple-music-svgrepo-com.svg?react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl">Chat Sumlin</h1>
+      <AppleMusicLogo height={15} /> <p> Now Playing </p>
+    </div>
+  );
+}
